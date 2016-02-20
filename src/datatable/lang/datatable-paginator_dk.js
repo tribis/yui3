7fca5d1dfb32bb212843e1b55ddad737df0b6d08
Paolo Nesti Poggi <pnp@eaktion.com>
2016-02-21 00:23:00 +0100

    datatable-paginator: added DK and IT texts

---
+++ src/datatable/lang/datatable-paginator_dk.js
@@ -0,0 +1,10 @@
+{
+    first: "Første",
+    prev: "Forrige",
+    next: "Næste",
+    last: "Sidste",
+    goToLabel: "Side:",
+    goToAction: "Gå",
+    perPage: "Rækker:",
+    showAll: "Vis alle"
+}
---
+++ src/datatable/lang/datatable-paginator_it.js
@@ -0,0 +1,11 @@
+{
+    first: "Prima",
+    prev: "Precedente",
+    next: "Prossima",
+    last: "Ultima",
+    goToLabel: "Pagina:",
+    goToAction: "Vai",
+    perPage: "Righe:",
+    showAll: "Tutte"
+}
+
